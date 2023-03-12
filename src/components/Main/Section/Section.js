a652c1f42dbe74e942a9c31d0d2b131659d9b4c9
VitaliyLomachinsky <lomachinsky@ukr.net>
2023-03-12 17:34:38 +0200

    23

--- src/components/Main/Section/Section.js
+++ src/components/Main/Section/Section.js
@@ -1,10 +1,43 @@
 import s from "./Section.module.css";
+import {useState} from "react"
+
 
 function Section() {
-    return <div className={s.section}>
-        <h2 className={s.section_title}>Constants<label for={"checker"} className={s.btn}></label></h2>
-        <input type={"checkbox"} class={s.checker} id={"checker"}></input>
-        <div className={s.text_wrapper}><p className={s.section_text}>asdasd</p></div>
+
+    const [isChecked, setIsChecked] = useState(false)
+
+    const checkHandler = () => {
+        setIsChecked(!isChecked)
+    }
+    return <div className={
+        s.section
+    }>
+        <h2 className={
+            s.section_title
+        }>
+            <label>Constants</label>
+            <label htmlFor={"checker"}
+                className={
+                    s.btn
+            }>
+                {
+                isChecked ? "less" : "more"
+            }</label>
+        </h2>
+        <input type={"checkbox"}
+            class={
+                s.checker
+            }
+            id={"checker"}
+            checked={isChecked}
+            onChange={checkHandler}></input>
+        <div className={
+            s.text_wrapper
+        }>
+            <p className={
+                s.section_text
+            }>asdasd</p>
+        </div>
 
     </div>
 }
