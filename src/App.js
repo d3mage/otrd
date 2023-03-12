473369cb3e56b82cdd1ca0e0b8398808fc4c1cc3
VitaliyLomachinsky <lomachinsky@ukr.net>
2023-03-13 00:35:53 +0200

    title

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+
 import Main from './components/Main/Main';
 
 function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-      <Main></Main>
-      <Footer></Footer>
-    </div>
-  );
+    return (
+        <>
+            <div className="App">
+                <Header></Header>
+                <Main></Main>
+
+            </div>
+
+        </>
+
+    );
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,34 @@ import s from './Header.module.css';
 import image from '../Header/image.png';
 
 function Header() {
-  return (
-    <header>
-      <div className={s.head}>
-        <h2 className={s.title}>Solidity</h2>
-        <img src={image} className={s.logo}></img>
-      </div>
-    </header>
-  );
+    return (
+        <header>
+            <div className={
+                s.head
+            }>
+                <div className={
+                    s.header_content
+                }>
+                    <h2 className={
+                        s.title
+                    }>Solidity</h2>
+                    <img src={image}
+                        className={
+                            s.logo
+                    }></img>
+
+
+                </div>
+                <div className={
+                    s.developers
+                }>
+                    <label>By Dmytro and Vitaliy PI-421</label>
+                </div>
+
+            </div>
+            <div></div>
+        </header>
+    );
 }
 
 export default Header;
