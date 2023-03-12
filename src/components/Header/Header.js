dc85ffe4252e9295f019d9caa8d22c128c2413cf
Dmytro Sotnyk <d.sotnyk@lacero.io>
2023-03-13 00:38:01 +0200

    points events

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,21 @@
 import s from './Header.module.css';
 import image from '../Header/image.png';
+import { useState } from 'react';
 
 function Header() {
+  const [result, setResult] = useState(
+    localStorage.getItem('result') === null
+      ? 0
+      : localStorage.getItem('result'),
+  );
+
+  window.addEventListener('result_increase', () => {
+    console.log('result_increase');
+    let temp = result;
+    setResult((temp += 5));
+    localStorage.setItem('result', result);
+  });
+
   return (
     <header>
       <div className={s.head}>
@@ -11,6 +25,9 @@ function Header() {
         </div>
         <div className={s.developers}>
           <label>By Dmytro and Vitaliy PI-421</label>
+        </div>
+        <div>
+          <h3>Your result: {result} Pts</h3>
         </div>
       </div>
       <div></div>
