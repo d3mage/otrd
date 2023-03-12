44759ce63c0a0a90d0ef9e522af1abaf8c9c929f
VitaliyLomachinsky <lomachinsky@ukr.net>
2023-03-12 17:34:30 +0200

    123

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
 
 function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-      <Main></Main>
-      <Footer></Footer>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header></Header>
+            <Main></Main>
+            <Footer></Footer>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,18 +6,15 @@ const data = [
         title: "Start",
         content: "pragma specifies the compiler version of Solidity.",
         status: false
-    },
-    {
+    }, {
         title: "Variables",
         content: "There are 3 types of variables in Solidity",
         status: false
-    },
-    {
+    }, {
         title: "Constants",
         content: "Constants are variables that cannot be modified.Their value is hard coded and using constants can save gas cost.",
         status: false
-    },
-    {
+    }, {
         title: "Immutable",
         content: "Immutable variables are like constants. Values of immutable variables can be set inside the constructor but cannot be modified afterwards.",
         status: false
@@ -25,9 +22,13 @@ const data = [
 ];
 
 function Main() {
-    return (<div className={s.main}>
-        <Section></Section>
-    </div>);
+    return (
+        <div className={
+            s.main
+        }>
+            <Section></Section>
+        </div>
+    );
 
 }
 
