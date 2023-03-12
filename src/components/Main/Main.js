db06a9992769fd5892fb33454879a0c09758da44
Dmytro Sotnyk <d.sotnyk@lacero.io>
2023-03-12 17:55:44 +0200

    edited section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,33 +5,29 @@ const data = [
   {
     title: 'Start',
     content: 'pragma specifies the compiler version of Solidity.',
-    status: false,
   },
   {
     title: 'Variables',
     content: 'There are 3 types of variables in Solidity',
-    status: false,
   },
   {
     title: 'Constants',
     content:
       'Constants are variables that cannot be modified.Their value is hard coded and using constants can save gas cost.',
-    status: false,
   },
   {
     title: 'Immutable',
     content:
       'Immutable variables are like constants. Values of immutable variables can be set inside the constructor but cannot be modified afterwards.',
-    status: false,
   },
 ];
 
+const sections = data.map((section) => (
+  <Section key={section.title} props={section}></Section>
+));
+
 function Main() {
-  return (
-    <div className={s.main}>
-      <Section></Section>
-    </div>
-  );
+  return <div className={s.main}>{sections}</div>;
 }
 
 export default Main;
--- src/components/Main/Section/Section.js
+++ src/components/Main/Section/Section.js
@@ -1,27 +1,27 @@
 import s from './Section.module.css';
 import { useState } from 'react';
 
-function Section() {
+function Section({ props }) {
   const [isChecked, setIsChecked] = useState(false);
 
   const checkHandler = () => {
     setIsChecked(!isChecked);
   };
+
+  let content;
+  if (isChecked) {
+    content = <h4>{props.content}</h4>;
+  }
+
   return (
     <div className={s.section}>
       <h2 className={s.section_title}>
-        <label>Constants</label>
-        <label htmlFor={'checker'} className={s.btn}>
-          {isChecked ? 'less' : 'more'}
-        </label>
+        <label>{props.title}</label>
+        <label onClick={checkHandler}>{isChecked ? 'less' : 'more'}</label>
       </h2>
-      <input
-        type={'checkbox'}
-        class={s.checker}
-        id={'checker'}
-        checked={isChecked}
-        onChange={checkHandler}
-      ></input>
+
+      {content}
+
       <div className={s.text_wrapper}>
         <p className={s.section_text}>asdasd</p>
       </div>
